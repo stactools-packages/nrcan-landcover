464a29307663dcd30490f2cc5eb0b03709dd1d89
bluegeo <devin.cairns@bluegeo.ca>
2021-07-12 17:31:13 -0700

    Add create-collection, write passing tests

--- README.md
+++ README.md
@@ -3,3 +3,37 @@
 Collection of Land Cover products for Canada as produced by Natural Resources Canada using Landsat satellite imagery. This collection of cartographic products offers classified Land Cover of Canada at a 30 metre spatial resolution, updated on a 5 year basis.
 
 This land cover dataset is the Canadian contribution to the 30 metre spatial resolution 2015 Land Cover Map of North America, which is produced by Mexican, American and Canadian government institutions under a collaboration called the North American Land Change Monitoring System (NALCMS).
+
+## Usage
+
+1. As a python module
+
+```python
+from stactools.nrcan_landcover.constants import JSONLD_HREF
+from stactools.nrcan_landcover import utils, cog, stac
+
+
+# Read metadata
+metadata = utils.get_metadata(JSONLD_HREF)
+
+# Create a STAC Collection
+json_path = os.path.join(tmp_dir, "/path/to/nrcan-landcover.json")
+stac.create_collection(metadata, json_path)
+
+# Create a COG
+cog.create_cog("/path/to/local.tif", "/path/to/cog.tif")
+
+# Create a STAC Item
+stac.create_item(metadata, "/path/to/item.json", "/path/to/cog.tif")
+```
+
+2. Using the CLI
+
+```bash
+# STAC Collection
+nrcanlandcover create-collection -d "/path/to/directory"
+# Create a COG - creates /path/to/local_cog.tif
+nrcanlandcover create-cog -d "/path/to/directory" -s "/path/to/local.tif"
+# Create a STAC Item - creates /path/to/directory/local_cog.json
+nrcanlandcover create-item -d "/path/to/directory" -c "/path/to/local_cog.tif"
+```
--- tests/test_stac.py
+++ tests/test_stac.py
@@ -5,9 +5,7 @@ import unittest
 import pystac
 
 from stactools.nrcan_landcover.constants import JSONLD_HREF
-from stactools.nrcan_landcover import utils
-from stactools.nrcan_landcover import cog
-from stactools.nrcan_landcover import stac
+from stactools.nrcan_landcover import utils, cog, stac
 
 from tests import test_data
 
@@ -17,18 +15,19 @@ class StacTest(unittest.TestCase):
         with TemporaryDirectory() as tmp_dir:
             test_path = test_data.get_path("data-files")
             paths = [
-                os.path.join(test_path, d)
-                for d in os.listdir(test_path)
+                os.path.join(test_path, d) for d in os.listdir(test_path)
                 if d.lower().endswith(".tif")
             ]
 
             for path in paths:
                 output_path = os.path.join(
-                    tmp_dir, os.path.basename(path)[:-4] + "_cog.tif"
-                )
+                    tmp_dir,
+                    os.path.basename(path)[:-4] + "_cog.tif")
                 cog.create_cog(path, output_path)
 
-                cogs = [p for p in os.listdir(tmp_dir) if p.endswith("_cog.tif")]
+                cogs = [
+                    p for p in os.listdir(tmp_dir) if p.endswith("_cog.tif")
+                ]
                 self.assertEqual(len(cogs), 1)
 
     def test_create_item(self):
@@ -47,3 +46,20 @@ class StacTest(unittest.TestCase):
             item = pystac.read_file(item_path)
 
         item.validate()
+
+    def test_create_collection(self):
+        with TemporaryDirectory() as tmp_dir:
+            metadata = utils.get_metadata(JSONLD_HREF)
+
+            # Create stac collection
+            json_path = os.path.join(tmp_dir, "test.json")
+            stac.create_collection(metadata, json_path)
+
+            jsons = [p for p in os.listdir(tmp_dir) if p.endswith(".json")]
+            self.assertEqual(len(jsons), 1)
+
+            collection_path = os.path.join(tmp_dir, jsons[0])
+
+            collection = pystac.read_file(collection_path)
+
+            collection.validate()
