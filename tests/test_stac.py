4e2e8d2f7b74077e2a40b5bde5a5b14d07430ad9
Tyler Battle <tyler@battle.ca>
2021-08-26 17:58:14 -0700

    Add more tests for proj and label extension

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -55,6 +55,7 @@ class StacTest(unittest.TestCase):
 
         assert "metadata" in item.assets
         assert "thumbnail" in item.assets
+        assert "data" in asset.roles
 
         # Projection Extension
         assert "proj:epsg" in item.properties
@@ -62,18 +63,39 @@ class StacTest(unittest.TestCase):
         assert "proj:transform" in item.properties
         assert "proj:shape" in item.properties
 
+        assert "proj:epsg" in asset.extra_fields
+        assert "proj:bbox" in asset.extra_fields
+        assert "proj:transform" in asset.extra_fields
+        assert "proj:shape" in asset.extra_fields
+
         # File Extension
         assert "file:size" in asset.extra_fields
         assert "file:values" in asset.extra_fields
         assert len(asset.extra_fields["file:values"]) > 0
-        assert "raster:bands" in asset.extra_fields
 
         # Raster Extension
+        assert "raster:bands" in asset.extra_fields
         assert len(asset.extra_fields["raster:bands"]) == 1
         assert "nodata" in asset.extra_fields["raster:bands"][0]
         assert "sampling" in asset.extra_fields["raster:bands"][0]
         assert "data_type" in asset.extra_fields["raster:bands"][0]
         assert "spatial_resolution" in asset.extra_fields["raster:bands"][0]
+
+        # Label Extension
+        assert "labels" in asset.roles
+        assert "labels-raster" in asset.roles
+
+        assert "label:type" in item.properties
+        assert "label:tasks" in item.properties
+        assert "label:properties" in item.properties
+        assert "label:description" in item.properties
+        assert "label:classes" in item.properties
+
+        assert "label:type" in asset.extra_fields
+        assert "label:tasks" in asset.extra_fields
+        assert "label:properties" in asset.extra_fields
+        assert "label:description" in asset.extra_fields
+        assert "label:classes" in asset.extra_fields
 
         item.validate()
 
@@ -94,7 +116,39 @@ class StacTest(unittest.TestCase):
 
             collection = pystac.read_file(collection_path)
 
+            item_asset = collection.extra_fields["item_assets"]["landcover"]
+            summaries = collection.summaries.to_dict()
+
             assert "metadata" in collection.assets
             assert "thumbnail" in collection.assets
+            assert "data" in item_asset["roles"]
+
+            # Projection Extension
+            assert "proj:epsg" in item_asset
+            assert "proj:epsg" in summaries
+
+            # File Extension
+            assert "file:values" in item_asset
+            assert len(item_asset["file:values"]) > 0
+
+            # Raster Extension
+            assert "raster:bands" in item_asset
+            assert "nodata" in item_asset["raster:bands"]
+            assert "sampling" in item_asset["raster:bands"]
+            assert "data_type" in item_asset["raster:bands"]
+            assert "spatial_resolution" in item_asset["raster:bands"]
+
+            # Label Extension
+            assert "labels" in item_asset["roles"]
+            assert "labels-raster" in item_asset["roles"]
+
+            assert "label:type" in summaries
+            assert "label:tasks" in summaries
+            assert "label:classes" in summaries
+
+            assert "label:type" in item_asset
+            assert "label:tasks" in item_asset
+            assert "label:properties" in item_asset
+            assert "label:classes" in item_asset
 
             collection.validate()
